9d7650b32e59bf75bf13770f7f489541395f7322
Sergio Mendoza <smendoza787@gmail.com>
2017-02-16 23:43:47 -0800

    finally got the timer algorithm right

--- pomodoro.js
+++ pomodoro.js
@@ -1,36 +1,65 @@
-var time = 5;
-var countdownFunction, update;
+var minutes, seconds, countdownFunction, update;
 var audio = new Audio('./ding.mp3');
 
+// Is the clock running?
+var isRunning = false;
+
+// This starts after start button is hit and basically upates forever
 function updateMessage() {
   var message = document.getElementById('message');
-  message.innerHTML = "Seconds left: " + time;
+  if (seconds < 10) {
+    message.innerHTML = "Minutes left: " + minutes + ":0" + seconds;
+  } else {
+    message.innerHTML = "Minutes left: " + minutes + ":" + seconds;
+  }
+
+  // When the minutes and seconds hit 0 stop the clock
+  if (seconds == 0 && minutes == 0) {
+    stop();
+    isRunning = false;
+  }
 }
 
-function countdown() {
-  time--;
 
-  if (time == 0) {
+function countdownMinutes() {
+  minutes--;
+}
+
+function countdownSeconds() {
+  seconds--;
+
+  if (seconds < 0 && minutes > 0) {
+    seconds = 59;
+    minutes--;
+  }
+
+  if (minutes == 0 && seconds == 0) {
     audio.play();
   }
-
-  if (time == -1) {
-    stopCountdown();
-  }
-}
-
-function stopCountdown() {
-  clearInterval(update);
-  clearInterval(countdownFunction);
 }
 
 function start() {
-  if (time !== 0) {
-    countdownFunction = setInterval(countdown, 1000);
-    update = setInterval(updateMessage, 500);
+  if (!minutes && !seconds) {
+    minutes = 0;
+    seconds = 10;
   }
+
+  if (isRunning === false && minutes >= 0) {
+    countdownSec = setInterval(countdownSeconds, 1000)
+    update = setInterval(updateMessage, 200);
+  }
+
+  isRunning = true;
 }
 
+function reset() {
+  stop();
+  minutes = 0;
+  seconds = 10;
+}
+
+
 function stop() {
-  stopCountdown();
+  clearInterval(countdownSec);
+  isRunning = false;
 }
