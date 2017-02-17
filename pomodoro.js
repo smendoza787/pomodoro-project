2e2b2df39c1351f5ad9e377d84428fc7540ddd18
Sergio Mendoza <smendoza787@gmail.com>
2017-02-17 00:07:01 -0800

    cleaned up code

--- pomodoro.js
+++ pomodoro.js
@@ -1,16 +1,14 @@
-var minutes, seconds, countdownFunction, update;
+var minutes, seconds;
+var isRunning = false;
 var audio = new Audio('./ding.mp3');
 
-// Is the clock running?
-var isRunning = false;
-
 // This starts after start button is hit and basically upates forever
-function updateMessage() {
-  var message = document.getElementById('message');
+function displayTime() {
+  var time = document.getElementById('time');
   if (seconds < 10) {
-    message.innerHTML = minutes + ":0" + seconds;
+    time.innerHTML = minutes + ":0" + seconds;
   } else {
-    message.innerHTML = minutes + ":" + seconds;
+    time.innerHTML = minutes + ":" + seconds;
   }
 
   // When the minutes and seconds hit 0 stop the clock
@@ -20,19 +18,18 @@ function updateMessage() {
   }
 }
 
-
-function countdownMinutes() {
-  minutes--;
-}
-
+// counts down seconds
 function countdownSeconds() {
   seconds--;
 
+  // when seconds get to "-1" AND the minutes left are still more than 0,
+  // revert to 59 seconds and subtract from the minutes left
   if (seconds < 0 && minutes > 0) {
     seconds = 59;
     minutes--;
   }
 
+  //
   if (minutes == 0 && seconds == 0) {
     audio.play();
   }
@@ -41,12 +38,13 @@ function countdownSeconds() {
 function start() {
   if (!minutes && !seconds) {
     minutes = 0;
-    seconds = 10;
+    seconds = 5;
   }
 
   if (isRunning === false && minutes >= 0) {
+    // weird how I don't put the var keyword and it still works?
     countdownSec = setInterval(countdownSeconds, 1000)
-    update = setInterval(updateMessage, 100);
+    update = setInterval(displayTime, 100);
   }
 
   isRunning = true;
